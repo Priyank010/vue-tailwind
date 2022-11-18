cca4ca325e14cf3b5ad32d62ec50170c9489958d
Priyank Desai <priyank@finflock.com>
2022-11-18 16:38:38 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="justify-center flex bg-red-400 items-center h-screen">
+    <div class="text-4xl">
+      Hey There! 👋🏼 This is Tailwind CSS.
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+};
+</script>
